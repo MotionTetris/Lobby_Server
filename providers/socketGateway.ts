d7f06be54a70a5f62f15d03e7b32151d7de28f43
o-ogie <wrld.worthy@gmail.com>
2024-01-26 10:48:20 +0000

    fix: socket.io cors Error

--- providers/socketGateway.ts
+++ providers/socketGateway.ts
@@ -2,7 +2,12 @@ import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody } from
 import { Server } from 'socket.io';
 import { GameRoomDTO } from 'src/room/DTO';
 
-@WebSocketGateway()
+@WebSocketGateway({
+    cors: {
+      origin: '*', // 모든 도메인에서의 접근을 허용합니다. 실제 사용 시에는 보안을 위해 구체적인 도메인을 명시하는 것이 좋습니다.
+      credentials: true, // 쿠키를 사용할 경우 true로 설정
+    }
+  })
 export class AppGateway {
     
   @WebSocketServer()
